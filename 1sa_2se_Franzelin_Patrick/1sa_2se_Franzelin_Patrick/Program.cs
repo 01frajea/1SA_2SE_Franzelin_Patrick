52fa68ca4bfbf8ed2621d86ba1221001344c6115
Schularbeit02 <Schularbeit02@rg-me.schule.suedtirol.it>
2018-03-06 11:37:13 +0100

    Projekt mit GIT verknüpft

---
+++ 1sa_2se_Franzelin_Patrick/1sa_2se_Franzelin_Patrick/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _1sa_2se_Franzelin_Patrick
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
